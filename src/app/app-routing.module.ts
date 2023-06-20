5ac1487381707e96461655956ddc394e2bca3e24
hannechichiheb@gmail.com <hannechichiheb@gmail.com>
2023-06-20 12:13:01 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllvitrineComponent } from './vitrine/allvitrine/allvitrine.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"vitrine",redirectTo:'/',pathMatch:'full'},
+  {path:"",component:AllvitrineComponent},
+  {path:"login",component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './vitrine/header/header.component';
+import { BodyComponent } from './vitrine/body/body.component';
+import { FooterComponent } from './vitrine/footer/footer.component';
+import { AllvitrineComponent } from './vitrine/allvitrine/allvitrine.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    AllvitrineComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
